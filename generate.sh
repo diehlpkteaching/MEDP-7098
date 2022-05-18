4c3791d81c8ca5e2bf6ccdf4398601e3d3cc529f
Patrick <patrickdiehl@lsu.edu>
2022-05-18 12:58:56 -0500

    Add Session 1

--- generate.sh
+++ generate.sh
@@ -3,3 +3,7 @@
 echo "Updating webpage"
 pandoc -s README.md --toc -c pandoc.css --metadata title="MEDP 7098" -o docs/index.html
 
+echo "Updating lecture slides"
+
+cd 1-intro-linux/ && jupyter nbconvert Session1.ipynb  --to slides --output-dir ../docs/
+
