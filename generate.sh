ab0dec1484caeb2518bf5625b84e1ccd420faae5
Patrick <patrickdiehl@lsu.edu>
2022-07-15 13:32:21 -0500

    Upload HPX tool slides

--- generate.sh
+++ generate.sh
@@ -7,4 +7,5 @@ echo "Updating lecture slides"
 
 cd 1-intro-linux/ && jupyter nbconvert Session1.ipynb  --to slides --output-dir ../docs/ && cd ..
 cd 2-intro-python/ && jupyter nbconvert Session2.ipynb  --to slides --output-dir ../docs/ && cd ..
-cd 3-examples-python/ && jupyter nbconvert Session3.ipynb  --to slides --output-dir ../docs/ 
+cd 3-examples-python/ && jupyter nbconvert Session3.ipynb  --to slides --output-dir ../docs/ && cd ..
+cd 25-intro-hpc-tools && jupyter nbconvert Session25.ipynb  --to slides --output-dir ../docs/ 
